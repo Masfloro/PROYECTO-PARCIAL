c0fa0142ac166137fb8606b1b2a55bcdec2416d0
Marcel <marcel241201@hotmail.com>
2024-04-06 13:49:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from interfaz import *
 from PyQt5 import QtCore
 from PyQt5.QtCore import QPropertyAnimation
 from PyQt5 import QtCore, QtGui, QtWidgets
+import ping3
 
 class MiApp(QtWidgets.QMainWindow):
 	def __init__(self):
@@ -13,7 +14,12 @@ class MiApp(QtWidgets.QMainWindow):
 		self.ui.btn_general.clicked.connect(lambda:self.ui.stackedWidget.setCurrentWidget(self.ui.mapa_general)) 
 		self.ui.btn_switch.clicked.connect(lambda:self.ui.stackedWidget.setCurrentWidget(self.ui.swtich)) 
 		self.ui.btn_pc.clicked.connect(lambda:self.ui.stackedWidget.setCurrentWidget(self.ui.pc))
-		
+		ping3.verbose_ping('192.168.1.36')
+		if ping3.ping('192.168.1.36') is not None:
+			self.ui.label_50.setStyleSheet("background-color:rgb(255,0,0)")
+
+	
+
 if __name__ == "__main__":
      app = QtWidgets.QApplication(sys.argv)
      mi_app = MiApp()
